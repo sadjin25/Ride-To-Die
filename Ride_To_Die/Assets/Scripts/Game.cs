205921f35d8da1bfa70d5fafe1a00dfc2d94e8a0
sadjin25 <qkrdnwls0101@naver.com>
2022-08-14 13:59:23 +0900

    add Frame Limit to 60

--- Ride_To_Die/Assets/Scripts/Game.cs
+++ Ride_To_Die/Assets/Scripts/Game.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    public Score scoreManager;
-    public PedalButton buttonManager;
+    private void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
 }
